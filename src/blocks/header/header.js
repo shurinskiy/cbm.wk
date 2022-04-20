5c975177361def4609ee1535e37f05b5acd5b247
_tango <shurinskiy@mail.ru>
2022-04-20 18:30:36 +0600

    draft of homepage

---
+++ src/blocks/header/header.js
@@ -0,0 +1,34 @@
+import scrollLock from 'scroll-lock';
+
+(() => {
+	const $shell = $('.header__menu');
+	const $toggle = $('.header__menu-toggle');
+
+	$toggle.on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$(this).toggleClass('opened');
+		$shell.toggleClass('opened');
+		
+		if($shell.hasClass('opened')) {
+			$shell.css({'max-width': parseInt($shell.css('max-width')) + scrollLock.getPageScrollBarWidth()});
+			scrollLock.disablePageScroll();
+			return;
+		}
+
+		$shell.removeAttr('style');
+		scrollLock.clearQueueScrollLocks();
+		scrollLock.enablePageScroll();
+	});
+
+	$(window).on('click', function(e) {
+		if($shell.hasClass('opened') && !e.target.closest('.header__menu')) {
+			e.preventDefault();
+			$toggle.toggleClass('opened');
+			$shell.toggleClass('opened').removeAttr('style');
+			scrollLock.clearQueueScrollLocks();
+			scrollLock.enablePageScroll();
+		}
+	});
+
+})();
---
+++ src/blocks/home/home.js
@@ -0,0 +1,12 @@
+import Swiper from 'swiper';
+
+(() => {
+
+	new Swiper(".home__slider", {
+		spaceBetween: 0,
+		slidesPerView: 1,
+		loop: true,
+	});
+
+
+})();
