50ede447a5730221975101b1072a7aa4c3643ad0
_tango <shurinskiy@mail.ru>
2022-04-22 21:34:24 +0600

    slider in adaptive

--- src/blocks/home/home.js
+++ src/blocks/home/home.js
@@ -1,15 +1,31 @@
 import Swiper, { Mousewheel } from 'swiper';
 
 (() => {
+	let swiper;
+	const breakpoint = window.matchMedia('(max-width:960px)');
 
-	new Swiper(".home__slider", {
-		modules: [Mousewheel],
-		slidesPerView: 1,
-		loop: true,
-		speed: 800,
-		direction: 'vertical',
-		mousewheel: true
-	});
+	const enableSwiper = () => {
+		swiper = new Swiper(".home__slider", {
+			modules: [Mousewheel],
+			slidesPerView: 1,
+			loop: true,
+			speed: 800,
+			direction: 'vertical',
+			mousewheel: true
+		});
+	}
+
+	const breakpointChecker = () => {
+		if (breakpoint.matches === true) {
+			if (swiper !== undefined ) swiper.destroy(true, false);
+			return;
+		} else if ( breakpoint.matches === false ) {
+			return enableSwiper();
+		}
+	}
+
+	breakpoint.addEventListener('change', breakpointChecker);
+	breakpointChecker();
 
 	$('.home__side[data-scrollable]').on('mousewheel', function(e) {
 		e.stopPropagation();
