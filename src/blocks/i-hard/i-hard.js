5acff385b6fd34d3b081209ec5cb6d7bac4f78b6
_tango <shurinskiy@mail.ru>
2022-05-04 22:49:28 +0600

    add page8.html

--- src/blocks/i-hard/i-hard.js
+++ src/blocks/i-hard/i-hard.js
@@ -1,15 +1,34 @@
 import Swiper from 'swiper';
 
 (() => {
-
 	new Swiper(".i-hard__items", {
 		spaceBetween: 30,
-		slidesPerView: 2.7,
-		slidesOffsetBefore: 15,
 		loop: true,
-		// observeParents: true,
-		// observer: true,
-
+		breakpoints: {
+			1430: { 
+				slidesPerView: 2.7,
+			},
+			1280: { 
+				slidesPerView: 2.5,
+			},
+			1100: { 
+				slidesPerView: 2.3,
+			},
+			1000: { 
+				slidesPerView: 2.0,
+			},
+			870: { 
+				slidesPerView: 1.8,
+			},
+			740: { 
+				slidesPerView: 1.6,
+			},
+			540: { 
+				slidesPerView: 1.3,
+			},
+			0: { 
+				slidesPerView: 1,
+			}
+		}
 	});
-
 })();
